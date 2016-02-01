c630fc26642fa74079c7554fa71ae5f01e9aff81
Vdeub <loicvdbts1@gmail.com>
2016-02-01 13:17:00 +0000

    Ionic angular

---
+++ Ionic-sample-Project/Ionic-angular/conference/www/js/services.js
@@ -0,0 +1,34 @@
+angular.module('starter.services', ['ngResource'])
+
+.service('ogury', function () {
+
+        return {
+            adToServe: function () {
+                var ad = {
+                    onAdEvent: function (event) {
+                        console.log('onAdEvent called here');
+                        if (event == 'AdFound') {
+                            console.log('AD FOUND');
+                            // If you want to do specific stuff when an ad is about to be displayed, even just a log
+                        } else if (event == 'AdClosed') {
+                            console.log('AD CLOSED');
+                            // If you want to do specific stuff when an ad is clicked or dismissed, even just a log
+                        }
+                    },
+                    onAdNotFound: function (error) {
+                        console.log('AD NOT FOUND');
+                        // If you want to do specific stuff when no ad to display, even just a log
+                    }
+                };
+
+                if (typeof CPresage !== "undefined") {
+                    CPresage.adToServe(ad.onAdEvent, ad.onAdNotFound);
+                }
+            }
+        };
+
+    })
+
+.factory('Session', function ($resource) {
+    return $resource('http://localhost:5000/sessions/:sessionId');
+});
