ba9c1124e6aefbba362532c6c952e86faca2228c
Vdeub <loicvdbts1@gmail.com>
2016-03-04 09:07:18 +0000

    Do not display Ogury ads on the AdClosed to avoid ads in a row

--- Android-sample-Project/myapp_Admob_Mediation/app/src/main/java/com/example/myapp/PresageCustomEvent.java
+++ Android-sample-Project/myapp_Admob_Mediation/app/src/main/java/com/example/myapp/PresageCustomEvent.java
@@ -4,12 +4,18 @@ package com.example.myapp;
  * Created by Loic on 01/02/16.
  */
     import android.content.Context;
+    import android.content.SharedPreferences;
     import android.os.Bundle;
     import android.os.Handler;
     import android.os.Message;
+    import android.util.Log;
+
     import com.google.android.gms.ads.AdRequest;
     import com.google.android.gms.ads.mediation.customevent.CustomEventInterstitial;
     import com.google.android.gms.ads.mediation.customevent.CustomEventInterstitialListener;
+
+    import java.util.Date;
+
     import io.presage.Presage;
     import io.presage.utils.IADHandler;
 
@@ -35,6 +41,7 @@ package com.example.myapp;
             public void onAdFound() {
                 mCustomlistener.onAdLoaded();
                 mCustomlistener.onAdOpened();
+                MainActivity.resetAdTime();
             }
 
             @Override
@@ -52,7 +59,14 @@ package com.example.myapp;
         @Override
         public void requestInterstitialAd(Context context, CustomEventInterstitialListener customEventInterstitialListener, String s, com.google.android.gms.ads.mediation.MediationAdRequest mediationAdRequest, Bundle bundle) {
             mCustomlistener = customEventInterstitialListener;
-            Presage.getInstance().adToServe(PRESAGE_INTERSTITIAL, mPresageHandler);
+            SharedPreferences pref = context.getSharedPreferences("lastAd", 0);
+            final long AdDisplayed= pref.getLong("lastAd", 0L);
+            Date today = new Date();
+            final long AdToDisplay = today.getTime();
+            Log.i("PresageCount", "The count is " + (AdToDisplay - AdDisplayed));
+            if (AdToDisplay - AdDisplayed > 6000) {
+                Presage.getInstance().adToServe(PRESAGE_INTERSTITIAL, mPresageHandler);
+            }
         }
 
         @Override
