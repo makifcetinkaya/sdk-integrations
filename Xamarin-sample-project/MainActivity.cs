0a5dce11471fb366b6b975f2ce477d5498703f93
Zhiheng <zhiheng@Zhihengs-MacBook-Pro-2.local>
2016-06-23 16:21:12 +0200

    update project for Sdk 1.8.1

--- Xamarin-sample-project/MainActivity.cs
+++ Xamarin-sample-project/MainActivity.cs
@@ -11,47 +11,155 @@ using IO.Presage;
 
 namespace example
 {
-	[Activity (Label = "example", MainLauncher = true, Icon = "@drawable/icon")]
+	[Activity(Label = "example", MainLauncher = true, Icon = "@drawable/icon")]
 	public class MainActivity : Activity
 	{
-		int count = 1;
-
-		protected override void OnCreate (Bundle bundle)
+		protected override void OnCreate(Bundle bundle)
 		{
-			base.OnCreate (bundle);
+			base.OnCreate(bundle);
 
 			Presage.Instance.Context = this.BaseContext;
-			Presage.Instance.Start ();
+			Presage.Instance.Start();
 
 			// Set our view from the "main" layout resource
-			SetContentView (Resource.Layout.Main);
+			SetContentView(Resource.Layout.Main);
 
 			// Get our button from the layout resource,
 			// and attach an event to it
-			Button button = FindViewById<Button> (Resource.Id.myButton);
-			
-			button.Click += delegate {
-				button.Text = string.Format ("{0} clicks!", count++);
+			Button adToServeButton = FindViewById<Button>(Resource.Id.adToServe);
+			adToServeButton.Click += OnAdToServeButtonClick;
+
+			Button showInterstitialButton = FindViewById<Button>(Resource.Id.showInterstitial);
+			showInterstitialButton.Click += ShowInterstitialClick;
+
+			Button launchWithEULAButton = FindViewById<Button>(Resource.Id.launchWithEULA);
+			launchWithEULAButton.Click += LaunchWithEULAButtonClick;
+
+		}
+
+
+
+		void LaunchWithEULAButtonClick(object sender, EventArgs ea)
+		{
+			var eulaEvents = Presage.Instance.LaunchWithEULA();
+
+			eulaEvents.EulaFound += (s, e) =>
+			{
+				System.Diagnostics.Debug.WriteLine("EulaFound");
+			};
+
+			eulaEvents.EulaNotFound += (s, e) =>
+			{
+				System.Diagnostics.Debug.WriteLine("EulaNotFound");
+			};
+
+			eulaEvents.EulaClosed += (s, e) =>
+			{
+				System.Diagnostics.Debug.WriteLine("EulaClosed");
 			};
 		}
 
-		protected override void OnResume ()
+		void ShowInterstitialClick(object sender, EventArgs ea)
 		{
-			base.OnResume ();
 
-			var interstitialEvents = Presage.Instance.AdToServe ("interstitial");
-				
-			interstitialEvents.AdClosed += (s, e) => {
-				System.Diagnostics.Debug.WriteLine ("Ad closed");
+			var loadEvents = Presage.Instance.LoadInterstitial();
+
+			loadEvents.AdClosed += (s, e) =>
+			{
+				System.Diagnostics.Debug.WriteLine("Ad closed");
 			};
 
-			interstitialEvents.AdFound += (s, e) => {
-				System.Diagnostics.Debug.WriteLine ("Ad found");
+
+			loadEvents.AdNotFound += (s, e) =>
+			{
+				System.Diagnostics.Debug.WriteLine("Ad not found");
 			};
 
-			interstitialEvents.AdNotFound += (s, e) => {
-				System.Diagnostics.Debug.WriteLine ("Ad not found");
+			loadEvents.AdError += (s, e) =>
+			{
+				System.Diagnostics.Debug.WriteLine("Ad error - code: " + e.Code);
 			};
+
+			loadEvents.AdDisplayed += (s, e) =>
+			{
+				System.Diagnostics.Debug.WriteLine("Ad displayed");
+			};
+
+			loadEvents.AdFound += (s, e) =>
+			{
+				System.Diagnostics.Debug.WriteLine("Ad found");
+				ShowInterstitial();
+			};
+
+
+		}
+
+		void ShowInterstitial()
+		{
+			var showEvents = Presage.Instance.ShowInterstitial();
+
+			showEvents.AdClosed += (st, et) =>
+			{
+				System.Diagnostics.Debug.WriteLine("Ad closed");
+			};
+
+			showEvents.AdFound += (st, et) =>
+			{
+				System.Diagnostics.Debug.WriteLine("Ad found");
+			};
+
+			showEvents.AdNotFound += (st, et) =>
+			{
+				System.Diagnostics.Debug.WriteLine("Ad not found");
+			};
+
+			showEvents.AdError += (st, et) =>
+			{
+				System.Diagnostics.Debug.WriteLine("Ad error - code: " + et.Code);
+			};
+
+			showEvents.AdDisplayed += (st, et) =>
+			{
+				System.Diagnostics.Debug.WriteLine("Ad displayed");
+			};
+		}
+
+
+
+		void OnAdToServeButtonClick(object sender, EventArgs ea)
+		{
+			var interstitialEvents = Presage.Instance.AdToServe("interstitial");
+
+			interstitialEvents.AdClosed += (s, e) =>
+			{
+				System.Diagnostics.Debug.WriteLine("Ad closed");
+			};
+
+			interstitialEvents.AdFound += (s, e) =>
+			{
+				System.Diagnostics.Debug.WriteLine("Ad found");
+			};
+
+			interstitialEvents.AdNotFound += (s, e) =>
+			{
+				System.Diagnostics.Debug.WriteLine("Ad not found");
+			};
+
+			interstitialEvents.AdError += (s, e) =>
+			{
+				System.Diagnostics.Debug.WriteLine("Ad error - code: " + e.Code);
+			};
+
+			interstitialEvents.AdDisplayed += (s, e) =>
+			{
+				System.Diagnostics.Debug.WriteLine("Ad displayed");
+			};
+		}
+
+
+		protected override void OnResume()
+		{
+			base.OnResume();
 		}
 	}
 }
